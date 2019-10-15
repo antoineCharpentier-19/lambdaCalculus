59f166e93038636004877906ec63d259e905d930
Hudson <acharpentier.44@gmail.com>
2019-10-15 16:00:33 +0200

    added first version of testsé

---
+++ src/test/java/ReduceByNameTest.java
@@ -0,0 +1,21 @@
+import model.Application;
+import model.Lambda;
+import model.Node;
+import model.Variable;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReduceByNameTest {
+
+    @Test
+    public void pureLambdaTest() {
+        Node node =
+                new Application(
+                        new Lambda("x", new Variable("x")),
+                        new Lambda("y", new Variable("y"))
+                );
+
+        assertEquals("(\\y -> y )", node.reduceByName().toString());
+    }
+}
